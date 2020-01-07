8d6fab04e664a96540f9954cd990325bd46741c3
Arthurmtro <arthurmtro@gmail.com>
2020-01-07 19:54:29 +0100

    Setting up

---
+++ build/precache-manifest.f0b0de02c3f118b262d93f797d745c96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e295c08d42523b005947644514a27ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30022aff2b195c632ff3",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "29aed0fd0d8e02ef6c1f",
+    "url": "/static/css/main.7f4fa8ee.chunk.css"
+  },
+  {
+    "revision": "30022aff2b195c632ff3",
+    "url": "/static/js/2.ed70ce6f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ed70ce6f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "29aed0fd0d8e02ef6c1f",
+    "url": "/static/js/main.50562690.chunk.js"
+  },
+  {
+    "revision": "cd6d4ac35bb3e9416556",
+    "url": "/static/js/runtime-main.e3012a12.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
